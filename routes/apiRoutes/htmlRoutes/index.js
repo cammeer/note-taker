a9b65d5f0e1c11ad2e5acd42061a19734b918b6e
cardinal <caren@merzhaus.org>
2022-07-02 21:13:55 -0400

    working on GET

--- routes/apiRoutes/htmlRoutes/index.js
+++ routes/apiRoutes/htmlRoutes/index.js
@@ -1,5 +1,14 @@
+const path = require("path");
 const router = require('express').Router();
 
+//GET
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+router.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
 
 
 
